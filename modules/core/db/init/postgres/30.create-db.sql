cf19b6dacb4233e968e7a3bfb5ab08720550ca08
Georgii Vlasov <g.vlasov@haulmont.com>
2021-05-21 19:50:16 +0400

    Anonimous user roles assignment during initialization

--- modules/core/db/init/postgres/30.create-db.sql
+++ modules/core/db/init/postgres/30.create-db.sql
@@ -1,1 +1,6 @@
-
+insert into SEC_USER_ROLE
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, USER_ID, ROLE_ID, ROLE_NAME)
+values ('3a09ee11-5492-56ee-d412-9993dc0d461c', 1, '2021-05-21 19:21:35', 'admin', '2021-05-21 19:21:35', null, null, null, 'a405db59-e674-4f63-8afe-269dda788fe8', null, 'self-user-registrated');
+insert into SEC_USER_ROLE
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, USER_ID, ROLE_ID, ROLE_NAME)
+values ('d2205e6c-7023-1e68-90df-aa6f1f174ed6', 1, '2021-05-21 19:21:35', 'admin', '2021-05-21 19:21:35', null, null, null, 'a405db59-e674-4f63-8afe-269dda788fe8', null, 'system-minimal');
