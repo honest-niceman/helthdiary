a973f3062d2e73a9f079ba731d8cb9e52b62b133
Georgii Vlasov <honest.niceman@gmail.com>
2021-05-22 02:01:30 +0400

    Graph screen correction

--- modules/web/src/com/company/helthdiary/web/screens/patientscreens/graphs/GraphScreen.java
+++ modules/web/src/com/company/helthdiary/web/screens/patientscreens/graphs/GraphScreen.java
@@ -36,10 +36,10 @@ public class GraphScreen extends Screen {
     private CollectionLoader<Pressure> pressuresDl;
     @Inject
     private CollectionContainer<Pulse> pulsesDc;
-
-    //todo переименуй чтобы был pulseSerialChart
     @Inject
-    private SerialChart Pulse;
+    private DateField<Date> startDate;
+    @Inject
+    private SerialChart pulseSerialChart;
     @Inject
     private CollectionLoader<Patient> patientsDl;
     @Inject
@@ -56,7 +56,35 @@ public class GraphScreen extends Screen {
 
     @Subscribe
     public void onInit(InitEvent event) {
+        initPatientAndUser(event);
 
+        initLoaders();
+
+        highlightCols();
+    }
+
+    @Subscribe("filterBtn")
+    public void onFilterBtnClick(Button.ClickEvent event) {
+        if (startDate.getValue() != null || endDate.getValue() != null) {
+            if (!startDate.getValue().before(endDate.getValue())) {
+                notifications.create().withCaption("Start date should be greate than end date").show();
+            } else {
+                Date start = startDate.getValue();
+                Date end = endDate.getValue();
+
+                loadGlucoses(start, end);
+                loadPressuress(start, end);
+                loadPulses(start, end);
+                loadTemperatures(start, end);
+
+                highlightCols();
+            }
+        } else {
+            notifications.create().withCaption("Input fields are empty").show();
+        }
+    }
+
+    private void initPatientAndUser(InitEvent event){
         ScreenOptions options = event.getOptions();
         if (options instanceof SelectedPatient) {
             patient = ((SelectedPatient) options).getPatient();
@@ -67,7 +95,9 @@ public class GraphScreen extends Screen {
             patient = patientsDl.getContainer().getItems().get(0);
             user = AppBeans.get(UserSessionSource.class).getUserSession().getUser();
         }
+    }
 
+    private void initLoaders(){
         pulsesDl.setParameter("user", user);
         pulsesDl.load();
 
@@ -79,56 +109,41 @@ public class GraphScreen extends Screen {
 
         temperaturesDl.setParameter("user", user);
         temperaturesDl.load();
-
-        highliteCols();
     }
 
-
-    @Subscribe("filterBtn")
-    public void onFilterBtnClick(Button.ClickEvent event) {
-        try {
-            if (startDate.getValue().before(endDate.getValue()) == false || startDate.getValue().after(endDate.getValue()) == true) {
-                notifications.create().withCaption("Check input dates").show();
-            } else if (startDate.getValue() != null && endDate.getValue() != null) {
-
-                Date start = startDate.getValue();
-                Date end = endDate.getValue();
-
-                glucoseDl.setQuery("select e from helthdiary_Glucose e where e.user = :user and e.date between :dateFirst and :dateLast");
-                glucoseDl.setParameter("user", user);
-                glucoseDl.setParameter("dateFirst", start);
-                glucoseDl.setParameter("dateLast", end);
-                glucoseDl.load();
-
-                pressuresDl.setQuery("select e from helthdiary_Pressure e where e.user = :user and e.date between :dateFirst and :dateLast");
-                pressuresDl.setParameter("user", user);
-                pressuresDl.setParameter("dateFirst", start);
-                pressuresDl.setParameter("dateLast", end);
-                pressuresDl.load();
-
-                pulsesDl.setQuery("select e from helthdiary_Pulse e where e.user = :user and e.date between :dateFirst and :dateLast");
-                pulsesDl.setParameter("user", user);
-                pulsesDl.setParameter("dateFirst", start);
-                pulsesDl.setParameter("dateLast", end);
-                pulsesDl.load();
-                highliteCols();
-
-                temperaturesDl.setQuery("select e from helthdiary_Temperature e where e.user = :user and e.date between :dateFirst and :dateLast");
-                temperaturesDl.setParameter("user", user);
-                temperaturesDl.setParameter("dateFirst", start);
-                temperaturesDl.setParameter("dateLast", end);
-                temperaturesDl.load();
-
-            }
-        } catch (NullPointerException ex) {
-            notifications.create().withCaption("Input fields are empty").show();
-        }
+    private void loadGlucoses(Date start, Date end) {
+        glucoseDl.setQuery("select e from helthdiary_Glucose e where e.user = :user and e.date between :dateFirst and :dateLast");
+        glucoseDl.setParameter("user", user);
+        glucoseDl.setParameter("dateFirst", start);
+        glucoseDl.setParameter("dateLast", end);
+        glucoseDl.load();
     }
 
-    @Inject
-    private DateField<Date> startDate;
+    private void loadPressuress(Date start, Date end) {
+        pressuresDl.setQuery("select e from helthdiary_Pressure e where e.user = :user and e.date between :dateFirst and :dateLast");
+        pressuresDl.setParameter("user", user);
+        pressuresDl.setParameter("dateFirst", start);
+        pressuresDl.setParameter("dateLast", end);
+        pressuresDl.load();
+    }
 
-    private void highliteCols() {
+    private void loadPulses(Date start, Date end) {
+        pulsesDl.setQuery("select e from helthdiary_Pulse e where e.user = :user and e.date between :dateFirst and :dateLast");
+        pulsesDl.setParameter("user", user);
+        pulsesDl.setParameter("dateFirst", start);
+        pulsesDl.setParameter("dateLast", end);
+        pulsesDl.load();
+    }
+
+    private void loadTemperatures(Date start, Date end) {
+        temperaturesDl.setQuery("select e from helthdiary_Temperature e where e.user = :user and e.date between :dateFirst and :dateLast");
+        temperaturesDl.setParameter("user", user);
+        temperaturesDl.setParameter("dateFirst", start);
+        temperaturesDl.setParameter("dateLast", end);
+        temperaturesDl.load();
+    }
+
+    private void highlightCols() {
         DataProvider dataProvider = new ListDataProvider();
 
         for (Pulse pulse : pulsesDc.getItems()) {
@@ -136,34 +151,15 @@ public class GraphScreen extends Screen {
             dataItem.put("rate", pulse.getRate());
             dataItem.put("date", pulse.getDate());
 
-            LocalDate dob;
+            Date patientDateOfBirth = patient.getDateOfBirth();
+
             LocalDate now = convertToLocalDateViaInstant(timeSource.currentTimestamp());
+            LocalDate localDateOfBirth = convertToLocalDateViaInstant(patientDateOfBirth);
 
-            Date rawDOB = patient.getDateOfBirth();
-            dob = convertToLocalDateViaInstant(rawDOB);
+            int age = Period.between(localDateOfBirth, now).getYears();
+            int normalRateMin = calculateNormalRateMin(age);
+            int normalRateMax = calculateNormalRateMax(age);
 
-            int age = Period.between(dob, now).getYears();
-            int normalRateMin = 0;
-            int normalRateMax = 10;
-
-            if (age >= 12 && age <= 15) {
-                normalRateMin = 68 - 10;
-                normalRateMax = 86 + 10;
-            } else if (age >= 16 && age <= 17) {
-                normalRateMin = 64 - 10;
-                normalRateMax = 80 + 10;
-            } else if (age >= 18 && age <= 50 && patient.getGender().equals(Gender.MALE)) {
-                normalRateMin = 60 - 10;
-                normalRateMax = 80 + 10;
-            } else if (age >= 18 && age <= 50 && patient.getGender().equals(Gender.FEMALE)) {
-                normalRateMin = 60 - 10;
-                normalRateMax = 70 + 10;
-            } else if (age >= 51) {
-                normalRateMin = 75 - 20;
-                normalRateMax = 90 + 10;
-            }
-
-            //todo добавь переменную final наверх и там укажи значение 70 и цвета назови
             if (pulse.getRate() >= normalRateMax) {
                 String color = pulse.getRate() >= normalRateMax ? "#FF0F00" : "#A9DEFF";
                 dataItem.put("color", color);
@@ -172,12 +168,44 @@ public class GraphScreen extends Screen {
                 dataItem.put("color", color);
             }
 
-
             dataProvider.addItem(new MapDataItem(dataItem));
         }
-        Pulse.setDataProvider(dataProvider);
+        pulseSerialChart.setDataProvider(dataProvider);
     }
 
+    private int calculateNormalRateMax(int age) {
+        int normalRateMax = 10;
+
+        if (age >= 12 && age <= 15) {
+            normalRateMax = 86 + 10;
+        } else if (age >= 16 && age <= 17) {
+            normalRateMax = 80 + 10;
+        } else if (age >= 18 && age <= 50 && patient.getGender().equals(Gender.MALE)) {
+            normalRateMax = 80 + 10;
+        } else if (age >= 18 && age <= 50 && patient.getGender().equals(Gender.FEMALE)) {
+            normalRateMax = 70 + 10;
+        } else if (age >= 51) {
+            normalRateMax = 90 + 10;
+        }
+        return normalRateMax;
+    }
+
+    private int calculateNormalRateMin(int age){
+        int normalRateMin = 0;
+
+        if (age >= 12 && age <= 15) {
+            normalRateMin = 68 - 10;
+        } else if (age >= 16 && age <= 17) {
+            normalRateMin = 64 - 10;
+        } else if (age >= 18 && age <= 50 && patient.getGender().equals(Gender.MALE)) {
+            normalRateMin = 60 - 10;
+        } else if (age >= 18 && age <= 50 && patient.getGender().equals(Gender.FEMALE)) {
+            normalRateMin = 60 - 10;
+        } else if (age >= 51) {
+            normalRateMin = 75 - 20;
+        }
+        return normalRateMin;
+    }
 
     public LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
         return dateToConvert.toInstant()
