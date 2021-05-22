16f5123851ac32c1ef62a93a7a700fcbf61163e0
Georgii Vlasov <g.vlasov@haulmont.com>
2021-05-22 17:07:11 +0400

    Roles correction

--- modules/core/src/com/company/helthdiary/core/role/DoctorRole.java
+++ modules/core/src/com/company/helthdiary/core/role/DoctorRole.java
@@ -1,35 +1,70 @@
 package com.company.helthdiary.core.role;
 
-import com.company.helthdiary.entity.patient.Pressure;
+import com.company.helthdiary.entity.doctor.Doctor;
+import com.company.helthdiary.entity.patient.*;
+import com.company.helthdiary.entity.visit.Visit;
 import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
-import com.haulmont.cuba.security.app.role.annotation.EntityAccess;
-import com.haulmont.cuba.security.app.role.annotation.EntityAttributeAccess;
-import com.haulmont.cuba.security.app.role.annotation.Role;
-import com.haulmont.cuba.security.app.role.annotation.ScreenAccess;
+import com.haulmont.cuba.security.app.role.annotation.*;
 import com.haulmont.cuba.security.entity.EntityOp;
 import com.haulmont.cuba.security.role.EntityAttributePermissionsContainer;
 import com.haulmont.cuba.security.role.EntityPermissionsContainer;
 import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
+import com.haulmont.cuba.security.role.SpecificPermissionsContainer;
 
 @Role(name = DoctorRole.NAME)
 public class DoctorRole extends AnnotatedRoleDefinition {
     public final static String NAME = "Doctor";
 
-    @EntityAccess(entityClass = Pressure.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
+    @EntityAccess(entityClass = Patient.class, operations = {EntityOp.READ})
+    @EntityAccess(entityClass = Pressure.class, operations = {EntityOp.READ})
+    @EntityAccess(entityClass = Pulse.class, operations = {EntityOp.READ})
+    @EntityAccess(entityClass = Glucose.class, operations = {EntityOp.READ})
+    @EntityAccess(entityClass = Temperature.class, operations = {EntityOp.READ})
+    @EntityAccess(entityClass = Visit.class, operations = {EntityOp.UPDATE, EntityOp.READ})
+    @EntityAccess(entityClass = Doctor.class, operations = {EntityOp.READ, EntityOp.UPDATE})
     @Override
     public EntityPermissionsContainer entityPermissions() {
         return super.entityPermissions();
     }
 
+    @EntityAttributeAccess(entityClass = Patient.class, modify = "*")
     @EntityAttributeAccess(entityClass = Pressure.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Glucose.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Temperature.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Visit.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Doctor.class, modify = "*")
     @Override
     public EntityAttributePermissionsContainer entityAttributePermissions() {
         return super.entityAttributePermissions();
     }
 
-    @ScreenAccess(screenIds = {"application-helthdiary", "helthdiary_RestorePasswordScreen", "helthdiary_Patient.browse"})
+    @ScreenAccess(screenIds = {
+            "application-helthdiary",
+            "helthdiary_All_Patients.browse",
+            "helthdiary_Doctor.browse",
+            "helthdiary_Doctor.edit",
+            "helthdiary_DoctorVisit.browse",
+            "helthdiary_GraphScreen",
+            "new-login",
+            "aboutWindow",
+            "help",
+            "settings",
+            "main",
+            "mainWindow"
+    })
     @Override
     public ScreenPermissionsContainer screenPermissions() {
         return super.screenPermissions();
+    }
+
+    @SpecificAccess(permissions = {
+            "cuba.gui.filter.customConditions",
+            "cuba.gui.filter.edit",
+            "cuba.gui.filter.global",
+            "cuba.gui.filter.maxResults"
+    })
+    @Override
+    public SpecificPermissionsContainer specificPermissions() {
+        return super.specificPermissions();
     }
 }
--- modules/core/src/com/company/helthdiary/core/role/PatientRole.java
+++ modules/core/src/com/company/helthdiary/core/role/PatientRole.java
@@ -4,28 +4,24 @@ import com.company.helthdiary.entity.doctor.Doctor;
 import com.company.helthdiary.entity.patient.*;
 import com.company.helthdiary.entity.visit.Visit;
 import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
-import com.haulmont.cuba.security.app.role.annotation.EntityAccess;
-import com.haulmont.cuba.security.app.role.annotation.EntityAttributeAccess;
-import com.haulmont.cuba.security.app.role.annotation.Role;
-import com.haulmont.cuba.security.app.role.annotation.ScreenAccess;
+import com.haulmont.cuba.security.app.role.annotation.*;
 import com.haulmont.cuba.security.entity.EntityOp;
 import com.haulmont.cuba.security.role.EntityAttributePermissionsContainer;
 import com.haulmont.cuba.security.role.EntityPermissionsContainer;
 import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
+import com.haulmont.cuba.security.role.SpecificPermissionsContainer;
 
-@Role(name = SelfUserRegistratedRole.NAME, isDefault = true)
-public class SelfUserRegistratedRole extends AnnotatedRoleDefinition {
-    public final static String NAME = "self-user-registrated";
+@Role(name = PatientRole.NAME, isDefault = true)
+public class PatientRole extends AnnotatedRoleDefinition {
+    public final static String NAME = "Patient";
 
-    @EntityAccess(entityClass = Patient.class, operations = {EntityOp.READ, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
+    @EntityAccess(entityClass = Patient.class, operations = {EntityOp.READ, EntityOp.UPDATE, EntityOp.CREATE})
     @EntityAccess(entityClass = Pressure.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
     @EntityAccess(entityClass = Pulse.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
     @EntityAccess(entityClass = Glucose.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
     @EntityAccess(entityClass = Temperature.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
     @EntityAccess(entityClass = Visit.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.DELETE, EntityOp.READ})
     @EntityAccess(entityClass = Doctor.class, operations = {EntityOp.READ})
-
-
     @Override
     public EntityPermissionsContainer entityPermissions() {
         return super.entityPermissions();
@@ -38,16 +34,50 @@ public class SelfUserRegistratedRole extends AnnotatedRoleDefinition {
     @EntityAttributeAccess(entityClass = Temperature.class, modify = "*")
     @EntityAttributeAccess(entityClass = Visit.class, modify = "*")
     @EntityAttributeAccess(entityClass = Doctor.class, modify = "*")
-
-
     @Override
     public EntityAttributePermissionsContainer entityAttributePermissions() {
         return super.entityAttributePermissions();
     }
 
-    @ScreenAccess(screenIds = {"helthdiary_RegisterScreen", "application-helthdiary", "helthdiary_Pressure.browse", "helthdiary_Pressure.edit", "helthdiary_RestorePasswordScreen", "helthdiary_Glucose.browse", "helthdiary_Pulse.browse", "helthdiary_Patient.browse", "helthdiary_Visit.browse", "helthdiary_GraphScreen", "helthdiary_Temperature.browse", "helthdiary_Doctor.browse1", "helthdiary_Doctor.browse"})
+    @ScreenAccess(screenIds = {
+            "application-helthdiary",
+            "new-login",
+            "helthdiary_RegisterScreen",
+            "helthdiary_RestorePasswordScreen",
+            "health_indicators",
+            "helthdiary_Patient.editAboutMe",
+            "helthdiary_Pressure.browse",
+            "helthdiary_Pressure.edit",
+            "helthdiary_Glucose.browse",
+            "helthdiary_Glucose.edit",
+            "helthdiary_Pulse.browse",
+            "helthdiary_Pulse.edit",
+            "helthdiary_Temperature.browse",
+            "helthdiary_Temperature.edit",
+            "helthdiary_Visit.browse",
+            "helthdiary_Visit.edit",
+            "helthdiary_GraphScreen",
+            "helthdiary_Doctor.browse1",
+            "aboutWindow",
+            "about_me_opener",
+            "help",
+            "settings",
+            "main",
+            "mainWindow"
+    })
     @Override
     public ScreenPermissionsContainer screenPermissions() {
         return super.screenPermissions();
+    }
+
+    @SpecificAccess(permissions = {
+            "cuba.gui.filter.customConditions",
+            "cuba.gui.filter.edit",
+            "cuba.gui.filter.global",
+            "cuba.gui.filter.maxResults"
+    })
+    @Override
+    public SpecificPermissionsContainer specificPermissions() {
+        return super.specificPermissions();
     }
 }
